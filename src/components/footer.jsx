e8f4d3ac29d45025a1bc399a40abdc9ae3dfd86d
shivamauryaGH <521241@student.nitandhra.ac.in>
2024-08-11 02:59:30 +0530

    some change in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,27 +15,27 @@ return <>
     </div>
 
     <div className="footer_column">
-        <h3>ONLINE SHOPPING</h3>
+        <h3>CUSTOMER POLICIES</h3>
 
-        <a href="#">Men</a>
-        <a href="#">Women</a>
-        <a href="#">Kids</a>
-        <a href="#">Home & Living</a>
-        <a href="#">Beauty</a>
-        <a href="#">Gift Card</a>
-        <a href="#">Myntra Insider</a>
+        <a href="#">Contact Us</a>
+        <a href="#">FAQ</a>
+        <a href="#">T&C</a>
+        <a href="#">Terms of Use</a>
+        <a href="#">Cancellation</a>
+        <a href="#">Returns</a>
+        <a href="#">Privacy policy</a>
     </div>
 
     <div className="footer_column">
-        <h3>ONLINE SHOPPING</h3>
+        <h3>DOWNLOAD OUR APP FROM</h3>
 
-        <a href="#">Men</a>
-        <a href="#">Women</a>
-        <a href="#">Kids</a>
-        <a href="#">Home & Living</a>
-        <a href="#">Beauty</a>
-        <a href="#">Gift Card</a>
-        <a href="#">Myntra Insider</a>
+        <a href="#">Google Play </a>
+        <a href="#">App Store</a>
+        <h3>KEEP IN TOUCH</h3>
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">You-Tube</a>
+        <a href="#">Instagram</a>
     </div>
 </div>
 <hr/>
